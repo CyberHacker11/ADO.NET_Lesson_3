134b5f03ba1f1b08395c9fa8b5c8e49597abe3fe
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-11-28 19:59:09 +0400

    Добавьте файлы проекта.

---
+++ Lesson_3/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Lesson_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxOrdersList = new System.Windows.Forms.ComboBox();
+            this.btnGetOrders = new System.Windows.Forms.Button();
+            this.btnOrderDetails = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxOrdersList
+            // 
+            this.cbxOrdersList.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbxOrdersList.FormattingEnabled = true;
+            this.cbxOrdersList.Location = new System.Drawing.Point(12, 12);
+            this.cbxOrdersList.Name = "cbxOrdersList";
+            this.cbxOrdersList.Size = new System.Drawing.Size(305, 31);
+            this.cbxOrdersList.TabIndex = 0;
+            // 
+            // btnGetOrders
+            // 
+            this.btnGetOrders.AutoSize = true;
+            this.btnGetOrders.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGetOrders.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnGetOrders.Location = new System.Drawing.Point(334, 10);
+            this.btnGetOrders.Name = "btnGetOrders";
+            this.btnGetOrders.Size = new System.Drawing.Size(113, 33);
+            this.btnGetOrders.TabIndex = 1;
+            this.btnGetOrders.Text = "Get Orders";
+            this.btnGetOrders.UseVisualStyleBackColor = true;
+            this.btnGetOrders.Click += new System.EventHandler(this.btnGetOrders_Click);
+            // 
+            // btnOrderDetails
+            // 
+            this.btnOrderDetails.AutoSize = true;
+            this.btnOrderDetails.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOrderDetails.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnOrderDetails.Location = new System.Drawing.Point(661, 10);
+            this.btnOrderDetails.Name = "btnOrderDetails";
+            this.btnOrderDetails.Size = new System.Drawing.Size(127, 33);
+            this.btnOrderDetails.TabIndex = 2;
+            this.btnOrderDetails.Text = "Order Details";
+            this.btnOrderDetails.UseVisualStyleBackColor = true;
+            this.btnOrderDetails.Click += new System.EventHandler(this.btnOrderDetails_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 377);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnOrderDetails);
+            this.Controls.Add(this.btnGetOrders);
+            this.Controls.Add(this.cbxOrdersList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxOrdersList;
+        private System.Windows.Forms.Button btnGetOrders;
+        private System.Windows.Forms.Button btnOrderDetails;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Lesson_3/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            path = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            table = new DataTable();
+            FillOrdersList();
+        }
+
+        private void FillOrdersList()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(path))
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Orders", connection);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cbxOrdersList.Items.Add(reader.GetString(1));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void btnGetOrders_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(path))
+                {
+                    connection.Open();
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT C.CustomerID, CompanyName FROM Orders AS O ");
+                    sb.Append("JOIN Customers AS C ");
+                    sb.Append("ON C.CustomerID = O.CustomerID ");
+                    sb.Append("WHERE O.CustomerID = @p1 ");
+                    sb.Append("GROUP BY C.CustomerID, CompanyName");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(sb.ToString(), connection);
+
+                    adapter.SelectCommand.Parameters.AddWithValue("@p1", cbxOrdersList.SelectedItem);
+
+                    DataTable table = new DataTable();
+
+                    adapter.Fill(table);
+
+                    dataGridView1.ClearSelection();
+
+                    dataGridView1.DataSource = table;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnOrderDetails_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(path))
+                {
+                    connection.Open();
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT OD.* FROM Orders AS O ");
+                    sb.Append("JOIN [Order Details] AS OD ");
+                    sb.Append("ON OD.OrderID = O.OrderID ");
+                    sb.Append("WHERE O.CustomerID = @p1 ");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(sb.ToString(), connection);
+
+                    adapter.SelectCommand.Parameters.AddWithValue("@p1", cbxOrdersList.SelectedItem);
+
+                    DataTable table = new DataTable();
+
+                    adapter.Fill(table);
+
+                    dataGridView1.ClearSelection();
+
+                    dataGridView1.DataSource = table;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        SqlCommandBuilder command;
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataTable table;
+        DataSet set;
+        string path;
+    }
+}
